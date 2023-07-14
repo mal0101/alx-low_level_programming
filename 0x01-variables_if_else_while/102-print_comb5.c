723ca1ce0ddc92563e4b6fa7ca0be392bd0f72b2
mal0101 <malakmekyassi04@gmail.com>
2023-07-14 15:59:27 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * check readme
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int x;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (x = i; x <= 99; x++)
+		{
+			int d11 = i / 10;
+			int d12 = i % 10;
+			int d21 = x / 10;
+			int d22 = x % 10;
+
+			putchar(d11 + '0');
+			putchar(d12 + '0');
+			putchar(',');
+			putchar(d21 + '0');
+			putchar(d22 + '0');
+
+			if (i != 99 || x != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
