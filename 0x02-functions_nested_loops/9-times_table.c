fc8ffaa107ed7daa80b23281f7e02b802323830f
mal0101 <malakmekyassi04@gmail.com>
2023-07-18 13:58:25 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table -Check description
+ * Description: it prints 9 times table starting with 0
+ * Return: Nothing
+ */
+void times_table(void)
+{
+	int i, j, x;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			x = i * j;
+
+			if ((x / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(x + '0');
+
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -34,3 +34,7 @@ Returns the value of the last digit
 
 Prototype: void jack_bauer(void);
 You can listen to this soundtrack while coding :)
+9- Write a function that prints the 9 times table, starting with 0.
+
+Prototype: void times_table(void);
+Format: see example
