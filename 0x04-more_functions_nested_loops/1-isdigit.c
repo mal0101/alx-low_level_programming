9c42c21ffb91b31568bfccb9c2a3cb0e9420128b
mal0101 <malakmekyassi04@gmail.com>
2023-07-20 10:46:46 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,9 +4,9 @@
  * @i: char to check
  * Return: 0 ou 1
  */
-int _isidigit(int i)
+int _isdigit(int i)
 {
-	if (c >= '0' && c <= '9')
+	if (i >= '0' && i <= '9')
 		return (1);
 	else
 		return (0);
