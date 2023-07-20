161772bbdaafe12a647ddd40103ae72e4cd838cf
mal0101 <malakmekyassi04@gmail.com>
2023-07-20 08:35:47 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper -checks for uppercase letters
+ * @x: char to check
+ * Return: 0 or 1
+ */
+int _isupper(int x)
+{
+	if (x >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
