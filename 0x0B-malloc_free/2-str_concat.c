9c0d924ac7ab11429fbaab4599d86faee7449ce8
mal0101 <malakmekyassi04@gmail.com>
2023-08-09 03:42:13 -0700

    He is who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,33 +1,42 @@
+#include "main.h"
 #include <stdlib.h>
 /**
- * *_strdup - a function that duplicates a string
- *
- * @str: input string to duplicate
- *
- * Return: NULL if str == NULL
- *         @str
-*/
-char *_strdup(char *str)
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
 {
-	int i = 0, l = 0;
-	char *s;
+	char *conct;
+	int i, ci;
 
-	if (str == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
+
+	if (conct == NULL)
 		return (NULL);
-
-	/*calculate size of str*/
-	while (str[l] != '\0')
-		l++;
-
-	s = malloc((l + 1) * sizeof(char));
-	if (s == NULL)
-		return (NULL);
-
-	while (str[i] != '\0')
+	i = ci = 0;
+	while (s1[i] != '\0')
 	{
-		s[i] = str[i];
+		conct[i] = s1[i];
 		i++;
 	}
 
-	return (s);
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++, ci++;
+	}
+	conct[i] = '\0';
+	return (conct);
 }
