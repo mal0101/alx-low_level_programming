cbf8fa9478ac2b3dc4f3f520687492468b128b21
Malak Mekyassi <malakmekyassi@192.168.1.3>
2023-11-27 21:39:31 +0100

    update 1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* read_textfile - read text file print to stdout
+* @filename: text file to be read
+* @letters: numbers of letters to be read
+* Return: w- actual number of bytes read and printed
+*	0 when function fails or filename is NULL*
+*/
